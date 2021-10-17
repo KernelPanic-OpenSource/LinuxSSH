a163de2b67888dc8da1c0c566334686082a1c7e0
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2021-10-18 01:26:56 +0700

    Create ngrok.sh

---
+++ ngrok.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo ""
+cho "=========================================="
+echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
+echo "=========================================="
